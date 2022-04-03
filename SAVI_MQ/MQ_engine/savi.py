06d9d7436d90c6e7c54d7867c22d50200dec4616
Abdallah Elshamy <abdallah.k.elshamy@gmail.com>
2022-04-03 03:22:47 -0400

    Add a helper function to run commands over ssh

--- SAVI_MQ/MQ_engine/savi.py
+++ SAVI_MQ/MQ_engine/savi.py
@@ -34,3 +34,49 @@ def getSSHSession(targetIP, username, password):
             time.sleep(5)
 
     return sshSession
+
+
+# Runs a command over an established SSH session
+#
+# Input:
+#   - sshSession: An active SSH session to a VM
+#   - command: A string command to run within the VM
+#
+# Returns:
+#   - A tuple containing strings of stdout and stderr (stdout, stderr), or
+#     else None if an exception occurred from SSH
+def runCommandOverSSH(sshSession, command):
+    assert type(sshSession) is paramiko.client.SSHClient,\
+            "'sshSession' is type %s" % type(sshSession)
+    assert type(command) in (str, unicode), "'command' is type %s" % type(command)
+    logger.debug("Running command in host %s" % sshSession._transport.sock.getpeername()[0])
+    logger.debug("\t\"%s\"" % command)
+
+    try:
+        stdin, stdout, stderr = sshSession.exec_command(command)
+
+        # Wait for command to finish (may take a while for long commands)
+        while not stdout.channel.exit_status_ready() or \
+                not stderr.channel.exit_status_ready():
+            time.sleep(1)
+    except Exception as e:
+        logger.error(e)
+        logger.error("ERROR: Unable to execute command over SSH:")
+        logger.error("\t%s" % command)
+
+        return None
+    else:
+        # exec_command() completed successfully
+        # Check if command printed anything to stderr
+        err = stderr.readlines()
+        err = ''.join(err) # Convert to single string
+        if err:
+            logger.error("%s\n" % err)
+
+        # Check if command printed anything to stdout
+        out = stdout.readlines()
+        out = ''.join(out) # Convert to single string
+        if out:
+            logger.debug("%s\n" % out)
+
+        return (out, err)
