d473b583f4b7b38d0eb8c96702d1a0fb99bdd1a6
Abdallah Elshamy <abdallah.k.elshamy@gmail.com>
2022-04-06 23:35:11 -0400

    Add a command to launch an MQ to the CLI tool

--- SAVI_MQ_CLI/savimq/cli.py
+++ SAVI_MQ_CLI/savimq/cli.py
@@ -1,9 +1,45 @@
 from typing import Optional
 import typer
 from . import __app_name__, __version__
+import json
 
+import sys, os
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', '..'))
+from savi import launch_mq, list_mqs, delete_mq, get_mq_info
 
 app = typer.Typer()
+
+
+@app.command()
+def launch(
+    config_file_path : str = typer.Argument(...),
+) -> None:
+    """Launches an MQ from a JSON config file."""
+    try:
+        f = open(config_file_path, "r")
+        config = json.loads(f.read())
+        launch_mq(config)
+        typer.echo("MQ Launched")
+    except IOError:
+        typer.secho(
+            'Could not open the specified file',
+            fg=typer.colors.RED,
+        )
+        raise typer.Exit(1)
+    except ValueError:
+        typer.secho(
+            'The specified config file does not contain valid JSON',
+            fg=typer.colors.RED,
+        )
+        raise typer.Exit(1)
+    except:
+        typer.secho(
+            'An error occured.',
+            fg=typer.colors.RED,
+        )
+        raise typer.Exit(1)
+    finally:
+        f.close()
 
 
 def _version_callback(value: bool) -> None:
